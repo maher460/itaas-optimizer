18a348851e4b262ca65dcc450aa0c625c301669a
Maher Khan <maher460@gmail.com>
2024-02-07 00:27:44 -0500

    updates

--- benchmark_sm2_pr_milp_optimizer.py
+++ benchmark_sm2_pr_milp_optimizer.py
@@ -3,8 +3,8 @@ from baas_utilities import *
 import time
 
 #num_apps_ops = [3, 5, 7, 9, 11, 13, 15, 20, 30, 50, 100]
-num_apps_ops = [5]
-num_list_per_size = 2
+num_apps_ops = [3, 5, 7, 9, 11, 13]
+num_list_per_size = 10
 
 aws_locations =     [("CO1", "Columbus, Ohio", (39.983334, -82.983330), 30), 
                      ("CO2", "Columbus, Ohio", (39.983334, -82.983330), 30), 
